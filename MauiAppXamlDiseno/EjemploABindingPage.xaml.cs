7554f528ec1f0f6bfc50c83e3f2c47e8b9a7a45c
joeltlc96 <145817552+joeltlc96@users.noreply.github.com>
2025-07-18 20:04:47 -0500

    9o

---
+++ MauiAppXamlDiseno/EjemploABindingPage.xaml.cs
@@ -0,0 +1,11 @@
+namespace MauiAppXamlDiseno;
+
+public partial class EjemploABindingPage : ContentPage
+{
+	public EjemploABindingPage()
+	{
+		InitializeComponent();
+	}
+} 
+
+private void btn_Clicked(object)
